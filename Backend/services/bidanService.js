2ead4652a5ad2009f1f597e7ebaa9ec7366b8d95
risya22008 <risyaannisachairyah1411@gmail.com>
2025-04-21 23:17:09 +0700

    Remove sendVerificationEmail function from bidanService

--- Backend/services/bidanService.js
+++ Backend/services/bidanService.js
@@ -1,8 +1,6 @@
 const bcrypt = require("bcrypt");
 const { db } = require("../firebaseClient");
-const jwt = require("jsonwebtoken");
-const { auth } = require("firebase-admin");
-const nodemailer = require("nodemailer");
+const jwt = require('jsonwebtoken');
 
 class BidanService {
     async createBidan(bidan) {
@@ -27,50 +25,7 @@ class BidanService {
 
 
         const bidanRef = await db.collection("Bidan").add(newBidan);
-        await this.sendVerificationEmail(bidan.email_bidan);
         return { id: bidanRef.id, ...newBidan };
-    }
-
-    async sendVerificationEmail(email) {
-        const actionCodeSettings = {
-            url: 'https://yourfrontend.com/verify', // frontend akan handle oobCode dari query string
-            handleCodeInApp: true
-        };
-
-        const verificationLink = await auth().generateEmailVerificationLink(email, actionCodeSettings);
-
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL_SENDER,
-                pass: process.env.EMAIL_PASSWORD
-            }
-        });
-
-        await transporter.sendMail({
-            from: `Tim Kesehatan <${process.env.EMAIL_SENDER}>`,
-            to: email,
-            subject: 'Verifikasi Email Anda',
-            html: `
-                <h3>Verifikasi Email</h3>
-                <p>Klik tombol di bawah ini untuk memverifikasi email Anda:</p>
-                <a href="${verificationLink}" style="padding: 10px 20px; background-color:rgb(48, 51, 212); color: white; text-decoration: none; border-radius: 5px;">Verifikasi Email</a>
-                <p>Atau salin dan tempel link ini di browser Anda:</p>
-                <p>${verificationLink}</p>
-            `
-        });
-
-        return verificationLink;
-    }
-
-    async updateVerifikasiEmail(email) {
-        const bidanSnapshot = await db.collection("Bidan").where("email_bidan", "==", email).get();
-        if (bidanSnapshot.empty) {
-            throw new Error("Pengguna tidak ditemukan");
-        }
-
-        const bidanDoc = bidanSnapshot.docs[0];
-        await bidanDoc.ref.update({ verifikasi_email: 2 });
     }
 
     async loginBidan(loginData) {
